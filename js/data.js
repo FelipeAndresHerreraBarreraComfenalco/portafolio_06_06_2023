772c3d735e547ab8c8135af699addd0c2ee93b55
FelipeAndresHerreraBarreraComfenalco <felhe057@gmail.com>
2023-06-21 17:43:40 -0500

    Avance json hasta skills

--- js/data.js
+++ js/data.js
@@ -40,5 +40,136 @@ addEventListener("DOMContentLoaded", async(e)=> {
             <a href="${data.about.parrafo.boton[0].link}" class="btn">${data.about.parrafo.boton[0].nombre}</a><span class="animate scroll" style="--i:5;"></span>
         </div>
     `);
-    document.querySelector("#")
+
+    document.querySelector(".education").insertAdjacentHTML("afterbegin", /*html*/`
+        <h2 class="heading">${data.education.titulo[0]} <span>${data.education.titulo[1]}</span><span class="animate scroll" style="--i:1;"></span></h2>
+    `);
+
+    document.querySelector("#mi_educacion").insertAdjacentHTML("afterbegin", /*html*/`
+        <h3 class="title">${data.education.education[0].nombre}<span class="animate scroll" style="--i:2;"></span></h3>
+
+        <div class="education-box">
+            <div class="education-content">
+                <div class="content">
+                    <div class="year"><i class='bx bxs-calendar'></i> ${data.education.education[1].año}</div>
+                    <h3>${data.education.education[1].institucion}</h3>
+                    <p>${data.education.education[1].descripcion}</p>
+                </div>
+            </div>
+
+            <div class="education-content">
+                <div class="content">
+                    <div class="year"><i class='bx bxs-calendar'></i> ${data.education.education[2].año}</div>
+                    <h3>${data.education.education[2].institucion}</h3>
+                    <p>${data.education.education[2].descripcion}</p>
+                </div>
+            </div>
+
+            <div class="education-content">
+                <div class="content">
+                    <div class="year"><i class='bx bxs-calendar'></i> ${data.education.education[3].año}</div>
+                    <h3>${data.education.education[3].institucion}</h3>
+                    <p>${data.education.education[3].descripcion}</p>
+                </div>
+            </div> 
+
+            <span class="animate scroll" style="--i:3;"></span>
+        </div>
+    `);
+
+    document.querySelector("#mi_experiencia").insertAdjacentHTML("afterbegin", /*html*/`
+        <h3 class="title">${data.education.experience[0].nombre}<span class="animate scroll" style="--i:5;"></span></h3>
+
+        <div class="education-box">
+            <div class="education-content">
+                <div class="content">
+                    <div class="year"><i class='bx bxs-calendar'></i> ${data.education.experience[1].año}</div>
+                    <h3>${data.education.experience[1].empresa}</h3>
+                    <p>${data.education.experience[1].descripcion}</p>
+                </div>
+            </div>
+
+            <div class="education-content">
+                <div class="content">
+                    <div class="year"><i class='bx bxs-calendar'></i> ${data.education.experience[2].año}</div>
+                    <h3>${data.education.experience[2].empresa}</h3>
+                    <p>${data.education.experience[2].descripcion}</p>
+                </div>
+            </div>
+
+            <div class="education-content">
+                <div class="content">
+                    <div class="year"><i class='bx bxs-calendar'></i> ${data.education.experience[3].año}</div>
+                    <h3>${data.education.experience[3].empresa}</h3>
+                    <p>${data.education.experience[3].descripcion}</p>
+                </div>
+            </div> 
+
+            <span class="animate scroll" style="--i:6;"></span>
+        </div>
+    `);
+
+    document.querySelector(".skills").insertAdjacentHTML("afterbegin", /*html*/`
+        <h2 class="heading">${data.skills.titulo[0]} <span>${data.skills.titulo[1]}</span><span class="animate scroll" style="--i:1;"></span></h2>
+    `);
+
+    document.querySelector("#mis_lenguajes").insertAdjacentHTML("afterbegin", /*html*/`
+        <h3 class="title">${data.skills.coding_skills[0].nombre}<span class="animate scroll" style="--i:2;"></span></h3>
+
+        <div class="skills-box">
+            <div class="skills-content">
+                <div class="progress">
+                    <h3>${data.skills.coding_skills[1].lenguage} <span>${data.skills.coding_skills[1].porcent}</span></h3>
+                    <div class="bar"><span></span></div>
+                </div>
+
+                <div class="progress">
+                    <h3>${data.skills.coding_skills[2].lenguage} <span>${data.skills.coding_skills[2].porcent}</span></h3>
+                    <div class="bar"><span></span></div>
+                </div>
+
+                <div class="progress">
+                    <h3>${data.skills.coding_skills[3].lenguage} <span>${data.skills.coding_skills[3].porcent}</span></h3>
+                    <div class="bar"><span></span></div>
+                </div>
+
+                <div class="progress">
+                    <h3>${data.skills.coding_skills[4].lenguage} <span>${data.skills.coding_skills[4].porcent}</span></h3>
+                    <div class="bar"><span></span></div>
+                </div>
+            </div>
+
+            <span class="animate scroll" style="--i:3;"></span>
+        </div>
+    `);
+
+    document.querySelector("#mis_habilidades").insertAdjacentHTML("afterbegin", /*html*/`
+        <h3 class="title">${data.skills.professional_skills[0].nombre}<span class="animate scroll" style="--i:5;"></span></h3>
+
+        <div class="skills-box">
+            <div class="skills-content">
+                <div class="progress">
+                    <h3>${data.skills.professional_skills[1].skill} <span>${data.skills.professional_skills[1].porcent}</span></h3>
+                    <div class="bar"><span></span></div>
+                </div>
+
+                <div class="progress">
+                    <h3>${data.skills.professional_skills[2].skill} <span>${data.skills.professional_skills[2].porcent}</span></h3>
+                    <div class="bar"><span></span></div>
+                </div>
+
+                <div class="progress">
+                    <h3>${data.skills.professional_skills[3].skill} <span>${data.skills.professional_skills[3].porcent}</span></h3>
+                    <div class="bar"><span></span></div>
+                </div>
+
+                <div class="progress">
+                    <h3>${data.skills.professional_skills[4].skill} <span>${data.skills.professional_skills[4].porcent}</span></h3>
+                    <div class="bar"><span></span></div>
+                </div>
+            </div>
+
+            <span class="animate scroll" style="--i:6;"></span>
+        </div>
+    `);
 })
